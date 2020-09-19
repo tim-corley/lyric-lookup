f12f846b222a7d7a63da05458c05eba811e7d6cd
Tim Corley <tcorley215@gmail.com>
2020-09-19 10:22:31 -0400

    adding spinner & conditional logic on Tracks

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,20 @@
 import React, { useContext } from "react";
 import { Context } from "../../context";
+import Spinner from "../Spinner";
 
 const Tracks = () => {
   const [state] = useContext(Context);
-  return (
-    <div className="text-center font-playfair text-indigo-800 text-xl">
-      <h1>Tracks</h1>
-    </div>
-  );
+  const { track_list, heading } = state;
+
+  if (track_list === undefined || track_list.length === 0) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="text-center font-playfair text-indigo-800 text-xl">
+        <h1>Tracks</h1>
+      </div>
+    );
+  }
 };
 
 export default Tracks;
